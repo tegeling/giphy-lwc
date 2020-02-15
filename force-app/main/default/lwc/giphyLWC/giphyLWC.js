97f78f12a6156430a8e9a9fa8379ea7e810f4635
Thomas Egeling <tegeling@tegeling-ltm1.internal.salesforce.com>
2020-02-15 15:53:14 +0100

    Initial Giphy LWC with static content

---
+++ force-app/main/default/lwc/giphyLWC/giphyLWC.js
@@ -0,0 +1,7 @@
+import { LightningElement } from 'lwc';
+
+export default class GiphyLWC extends LightningElement {
+    // Expose the content asset URL for use in the template
+    giphyContentUrl = "https://media0.giphy.com/media/lMCRocI1DvLbduqwWu/giphy.gif?cid=1b06eedd8e5886d3426571f3b550df75b93d08a806327393&rid=giphy.gif";
+
+}
