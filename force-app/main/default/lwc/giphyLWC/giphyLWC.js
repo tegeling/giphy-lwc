15b6d8bc082fd03966cae9ffe82c47f1c397f412
Thomas Egeling <tegeling@tegeling-ltm1.internal.salesforce.com>
2020-02-16 13:39:05 +0100

    Static search term implemented

--- force-app/main/default/lwc/giphyLWC/giphyLWC.js
+++ force-app/main/default/lwc/giphyLWC/giphyLWC.js
@@ -1,7 +1,54 @@
-import { LightningElement } from 'lwc';
+/* eslint-disable no-console */
+import { LightningElement, api, track } from 'lwc';
+
+const QUERY_URL =
+    'https://api.giphy.com/v1/gifs/search?';
 
 export default class GiphyLWC extends LightningElement {
-    // Expose the content asset URL for use in the template
-    giphyContentUrl = "https://media0.giphy.com/media/lMCRocI1DvLbduqwWu/giphy.gif?cid=1b06eedd8e5886d3426571f3b550df75b93d08a806327393&rid=giphy.gif";
+    @api giphyType;
+    @api staticURL;
+    @api giphyRating;
+    @api giphyAPIKey;
+    @api staticSearchTerm;
+    @api dynamicObject;
+    @api dynamicField;
+
+    query;
+    @track error;
+    @track giphyContentUrl;
+
+    connectedCallback() {
+        // check the Giphy source type (static URL, static search term, dynamic search term)
+        this.query = QUERY_URL + 'api_key=' + this.giphyAPIKey + '&limit=1&offset=0&rating=' + this.giphyRating + '&lang=en';
+        if (this.giphyType === 'Static Search Term') {
+            this.query += '&q=' + this.staticSearchTerm;
+            // The Fetch API is currently not polyfilled for usage in IE11.
+            // Use XMLHttpRequest instead in that case.
+            fetch(this.query)
+                .then(response => {
+                    // fetch isn't throwing an error if the request fails.
+                    // Therefore we have to check the ok property.
+                    if (!response.ok) {
+                        this.error = response;
+                    }
+                    return response.json();
+                })
+                .then(jsonResponse => {
+                    jsonResponse.data
+                    .map(gif => gif.images.fixed_height.url)
+                    .forEach(url => {
+                        this.giphyContentUrl = url;
+                        })
+                })
+                .catch(error => {
+                    this.error = error;
+                    console.log('**** error: ' + this.error);
+                });
+
+        } else if (this.giphyType === 'Static URL') {
+            this.giphyContentUrl = this.staticURL;
+        }
+    }
+    
 
 }
